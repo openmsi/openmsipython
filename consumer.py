88879120be56d261bba787783821346a0d4dec1f
Sam Tabrisky <samj.tabs@gmail.com>
2020-07-11 23:17:07 -0400

    Initial commit

---
+++ consumer.py
@@ -0,0 +1,44 @@
+import uuid
+
+from confluent_kafka import Producer, Consumer
+
+
+
+
+c = Consumer({
+    'bootstrap.servers': 'pkc-ep9mm.us-east-2.aws.confluent.cloud:9092',
+    'sasl.mechanism': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': '5AZU24G7K7AKNSYS',
+    'sasl.password': '6H8nMfsoeqJsoBShvtC5GIWHOS6U8La22JDKrWI2BT8wZWKi8qTTHrC3ygFueC2S',
+    'group.id': str(uuid.uuid1()),  # this will create a new consumer group on each invocation.
+    'auto.offset.reset': 'earliest'
+})
+
+c.subscribe(['users'])
+
+try:
+    while True:
+        msg = c.poll(0.1)  # Wait for message or event/error
+        if msg is None:
+            # No message available within timeout.
+            # Initial message consumption may take up to `session.timeout.ms` for
+            #   the group to rebalance and start consuming.
+            continue
+        if msg.error():
+            # Errors are typically temporary, print error and continue.
+            print("Consumer error: {}".format(msg.error()))
+            continue
+        # write message to file instead of printing
+        # print(msg.value())
+        filecontents = msg.value().decode()
+        file = open("output_test", 'w')
+        file.write(filecontents)
+        print('consumed: {}'.format(msg.value()))
+        
+except KeyboardInterrupt:
+    pass
+
+finally:
+    # Leave group and commit final offsets
+    c.close()
