a2a648fe1ac62c766409d71c8c46f85f165d58ee
Maggie Eminizer <margaret.eminizer@gmail.com>
2021-04-05 12:56:01 -0400

    add retries to the producer by default

--- my_kafka/my_producers.py
+++ my_kafka/my_producers.py
@@ -2,16 +2,23 @@
 
 #imports
 from confluent_kafka import Producer
+from ..utilities.misc import populated_kwargs
 from ..utilities.config import TUTORIAL_CLUSTER_CONST
 
 #Producer to the tutorial cluster
 class TutorialClusterProducer(Producer) :
 
-    def __init__(self) :
+    def __init__(self,**kwargs) :
+        """
+        Possible keywork arguments:
+        retries : the number of times to retry sending any particular message before erroring out (default is 2)
+        """
+        kwargs = populated_kwargs(kwargs,{'retries':2,})
         config = {'bootstrap.servers': TUTORIAL_CLUSTER_CONST.SERVER,
                   'sasl.mechanism'   : TUTORIAL_CLUSTER_CONST.SASL_MECHANISM,
                   'security.protocol': TUTORIAL_CLUSTER_CONST.SECURITY_PROTOCOL,
                   'sasl.username'    : TUTORIAL_CLUSTER_CONST.USERNAME,
-                  'sasl.password'    : TUTORIAL_CLUSTER_CONST.PASSWORD
+                  'sasl.password'    : TUTORIAL_CLUSTER_CONST.PASSWORD,
+                  'retries'          : kwargs['retries']
                 }
         super().__init__(config)
