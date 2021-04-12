417a3fe60dac13d8a1794e59512ce7971ce8e7eb
Maggie Eminizer <margaret.eminizer@gmail.com>
2021-04-12 14:27:54 -0400

    optimize consumer for throughput

--- my_kafka/my_consumers.py
+++ my_kafka/my_consumers.py
@@ -14,10 +14,12 @@ class TutorialClusterConsumer(Consumer) :
         Possible keyword arguments:
         group_id          = ID for the group that this consumer should belong to
         auto_offset_reset = argument for auto.offset.reset for this consumer
+        fetch_min_bytes   = argument for fetch.min.bytes for this consumer
         """
         kwargs = populated_kwargs(kwargs,
                                   {'group_id':str(uuid.uuid1()), # this will create a new consumer group on each invocation by default
-                                   'auto_offset_reset':'earliest'
+                                   'auto_offset_reset':'earliest',
+                                   'fetch_min_bytes':100000
                                   })
         config = {'bootstrap.servers': TUTORIAL_CLUSTER_CONST.SERVER,
                   'sasl.mechanism'   : TUTORIAL_CLUSTER_CONST.SASL_MECHANISM,
@@ -25,6 +27,7 @@ class TutorialClusterConsumer(Consumer) :
                   'sasl.username'    : TUTORIAL_CLUSTER_CONST.USERNAME,
                   'sasl.password'    : TUTORIAL_CLUSTER_CONST.PASSWORD,
                   'group.id'         : kwargs['group_id'],  
-                  'auto.offset.reset': kwargs['auto_offset_reset']
+                  'auto.offset.reset': kwargs['auto_offset_reset'],
+                  'fetch.min.bytes'  : kwargs['fetch_min_bytes']
                 }
         super().__init__(config)
