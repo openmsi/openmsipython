4032cec01a0ef91ed93b2c7f9683477a7ec325d6
Maggie Eminizer <margaret.eminizer@gmail.com>
2021-04-12 14:49:10 -0400

    logging is thread-safe, actually (cleaning up extra stuff)

--- data_file_io/utilities.py
+++ data_file_io/utilities.py
@@ -1,11 +1,11 @@
 #Several utility functions
 
 #a helper function to produce every file chunk in a given queue to the given topic using the given producer
-def produce_from_queue_of_file_chunks(queue,producer,topic_name,logger,lock) :
+def produce_from_queue_of_file_chunks(queue,producer,topic_name,logger) :
     #upload all the file chunks in the queue in a single thread
     file_chunk = queue.get()
     while file_chunk is not None :
-        file_chunk.upload_as_message(producer,topic_name,logger,lock)
+        file_chunk.upload_as_message(producer,topic_name,logger)
         queue.task_done()
         file_chunk = queue.get()
     queue.task_done()
