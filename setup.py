4f93d5335ee45016492b01a3aba42ad47af5eecc
Maggie Eminizer <margaret.eminizer@gmail.com>
2021-05-05 11:05:08 -0400

    "dmrefpython" -> "openmsipython"

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import setuptools, site
 site.ENABLE_USER_SITE = True #https://www.scivision.dev/python-pip-devel-user-install/
 
 setuptools.setup(
-    name='dmrefpython',
+    name='openmsipython',
     version='0.0.1',
-    packages=setuptools.find_packages(include=['dmrefpython*']),
+    packages=setuptools.find_packages(include=['openmsipython*']),
     entry_points = {
-        'console_scripts' : ['upload_data_files_added_to_directory=dmrefpython.command_line_scripts.upload_data_files_added_to_directory:main',
-                             'upload_data_file=dmrefpython.command_line_scripts.upload_data_file:main',
-                             'reconstruct_data_files=dmrefpython.command_line_scripts.reconstruct_data_files:main',
+        'console_scripts' : ['upload_data_files_added_to_directory=openmsipython.command_line_scripts.upload_data_files_added_to_directory:main',
+                             'upload_data_file=openmsipython.command_line_scripts.upload_data_file:main',
+                             'reconstruct_data_files=openmsipython.command_line_scripts.reconstruct_data_files:main',
                             ],
     },
     python_requires='>=3.7,<3.8',
