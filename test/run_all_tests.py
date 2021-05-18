e262f2151118f7c9dd7fcd1e63cd06f2c3540b00
Maggie Eminizer <margaret.eminizer@gmail.com>
2021-05-18 15:23:21 -0400

    add a test to make sure that the Git repository is up to date and unchanged after all other tests run (WIP)

--- test/run_all_tests.py
+++ test/run_all_tests.py
@@ -4,6 +4,7 @@ import subprocess, pathlib
 #constants
 TOP_DIR_PATH = (pathlib.Path(__file__).parent.parent).resolve()
 UNITTEST_DIR_PATH = (pathlib.Path(__file__).parent / 'unittests').resolve()
+TEST_REPO_STATUS_SCRIPT_PATH = (pathlib.Path(__file__).parent / 'test_repo_status.sh').resolve()
 CWD = pathlib.Path().resolve()
 
 def main() :
@@ -21,6 +22,13 @@ def main() :
     except subprocess.CalledProcessError as e :
         raise RuntimeError(f'ERROR: some unittest(s) failed with output:\n{e.output.decode()}')
     print('All unittest checks complete : )')
+    #make sure the Github repo is still clean from its initial state
+    print('Checking the status of the Git repo....')
+    p = subprocess.Popen(f'cd {TOP_DIR_PATH}; sh {TEST_REPO_STATUS_SCRIPT_PATH}; cd {CWD}; exit 0',stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+    stdout,stderr = p.communicate()
+    if stdout.decode()!='' :
+        raise RuntimeError(f'ERROR: Git repo check failed with output:\n{stdout.decode()}')
+    print('Repo is good : )')
 
 if __name__=='__main__' :
     main()
