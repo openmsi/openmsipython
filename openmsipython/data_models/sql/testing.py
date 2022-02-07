afeeebeba110be702675784f04f54f7d99f10bf1
Maggie Eminizer <margaret.eminizer@gmail.com>
2022-02-07 17:37:45 -0500

    testing 2

--- openmsipython/data_models/sql/testing.py
+++ openmsipython/data_models/sql/testing.py
@@ -12,19 +12,16 @@ DB_NAME = 'OpenMSI'
 connection_string=f'mssql+pymssql://{UNAME}:{PWD}@{HOST}/{DB_NAME}'
 engine = create_engine(connection_string,echo=True)
 
-print(f'CONNECTION STRING = {connection_string}')
-con = engine.connect()
-
-##print out the schemas in the database
-#sql = """
-#SELECT s.name AS schema_name, 
-#       s.schema_id,
-#       u.name AS schema_owner
-#FROM sys.schemas s
-#    INNER JOIN sys.sysusers u
-#        ON u.uid = s.principal_id
-#ORDER BY s.name
-#"""
-#with engine.connect() as con:
-#    r = con.execute(sql)
-#print(f'result = {r}')
+#print out the schemas in the database
+sql = """
+SELECT s.name AS schema_name, 
+       s.schema_id,
+       u.name AS schema_owner
+FROM sys.schemas s
+    INNER JOIN sys.sysusers u
+        ON u.uid = s.principal_id
+ORDER BY s.name
+"""
+with engine.connect() as con:
+    r = con.execute(sql)
+print(f'result = {r}')
