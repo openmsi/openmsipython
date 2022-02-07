bf6d63ae74e0f23809e7b7ecad410602655cffd6
Maggie Eminizer <margaret.eminizer@gmail.com>
2022-02-07 17:28:35 -0500

    adding a folder and a testing script for SQL stuff
    and some new package dependencies

---
+++ openmsipython/data_models/sql/testing.py
@@ -0,0 +1,27 @@
+#imports
+import os
+from sqlalchemy import create_engine
+
+#constants
+UNAME = 'openmsi_writer'
+PWD = os.path.expandvars('$OPENMSI_WRITER_PWORD')
+HOST = 'dsp056'
+DB_NAME = 'OpenMSI'
+
+#create the engine
+connection_string=f'mssql+pymssql://{UNAME}:{PWD}@{HOST}/{DB_NAME}'
+engine = create_engine(connection_string,echo=True)
+
+#print out the schemas in the database
+sql = """
+SELECT s.name AS schema_name, 
+       s.schema_id,
+       u.name AS schema_owner
+FROM sys.schemas s
+    INNER JOIN sys.sysusers u
+        ON u.uid = s.principal_id
+ORDER BY s.name
+"""
+with engine.connect() as con:
+    r = con.execute(sql)
+print(f'result = {r}')
