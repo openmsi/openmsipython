0b982d1750ac28244277642084066782c0955d89
Maggie Eminizer <margaret.eminizer@gmail.com>
2022-02-08 15:22:32 -0500

    debugging

--- openmsipython/data_models/sql/testing.py
+++ openmsipython/data_models/sql/testing.py
@@ -1,20 +1,24 @@
 #imports
 from .utilities import get_engine
 
-#create the engine
-engine = get_engine()
+def main() :
+    #create the engine
+    engine = get_engine()
 
-#print out the schemas in the database
-sql = """
-SELECT s.name AS schema_name, 
-       s.schema_id,
-       u.name AS schema_owner
-FROM sys.schemas s
-    INNER JOIN sys.sysusers u
-        ON u.uid = s.principal_id
-ORDER BY s.name
-"""
-with engine.connect() as con:
-    rs = con.execute(sql)
-    for r in rs :
-        print(r)
+    #print out the schemas in the database
+    sql = """
+    SELECT s.name AS schema_name, 
+        s.schema_id,
+        u.name AS schema_owner
+    FROM sys.schemas s
+        INNER JOIN sys.sysusers u
+            ON u.uid = s.principal_id
+    ORDER BY s.name
+    """
+    with engine.connect() as con:
+        rs = con.execute(sql)
+        for r in rs :
+            print(r)
+
+if __name__=='__main__' :
+    main()
--- openmsipython/data_models/sql/utilities.py
+++ openmsipython/data_models/sql/utilities.py
@@ -1,5 +1,4 @@
 #imports
-from multiprocessing.dummy import connection
 from sqlalchemy import create_engine
 from .config import SQL_CONST
 
@@ -7,5 +6,6 @@ def get_engine() :
     #create asnd return the engine
     connection_string=f'mssql+pymssql://{SQL_CONST.SQL_UNAME}:{SQL_CONST.SQL_PWD}'
     connection_string+=f'@{SQL_CONST.SQL_HOST}/{SQL_CONST.SQL_DB_NAME}'
+    print(f'connection_string = {connection_string}')
     engine = create_engine(connection_string,echo=True)
     return engine
