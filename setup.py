51628630c2ed0d4ef17f0b48f520c0b73cad2f9e
Maggie Eminizer <margaret.eminizer@gmail.com>
2022-05-03 13:28:38 -0400

    increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ site.ENABLE_USER_SITE = True #https://www.scivision.dev/python-pip-devel-user-in
 
 setupkwargs = dict(
     name='openmsipython',
-    version='0.8.4',
+    version='0.8.5',
     packages=setuptools.find_packages(include=['openmsipython*']),
     include_package_data=True,
     entry_points = {
