458e7d1f4680ffa46e93fa8d503e92ddc8732fc3
Amirhossein Sharifzadeh <amirsharifzadeh@gmail.com>
2022-05-20 13:54:55 -0400

    more changes

--- openmsipython/osn/osn_stream_processor.py
+++ openmsipython/osn/osn_stream_processor.py
@@ -1,12 +1,10 @@
-import datetime
-
 from openmsipython.data_file_io.config import RUN_OPT_CONST
 from openmsipython.data_file_io.data_file_stream_processor import DataFileStreamProcessor
 from openmsipython.osn.s3_data_transfer import s3_data_transfer
 from openmsipython.pdv.config import LECROY_CONST
 from openmsipython.pdv.lecroy_data_file import DownloadLecroyDataFile
 from openmsipython.shared.runnable import Runnable
-
+import logging
 
 class osn_stream_processor(DataFileStreamProcessor, Runnable):
 
@@ -22,7 +20,8 @@ class osn_stream_processor(DataFileStreamProcessor, Runnable):
         self.__header_rows = header_rows
         self.__osn_config = super().get_osn_config()
         self.__osn_config['bucket_name'] = bucket_name
-
+        self.bucket_name = bucket_name
+        self.s3d = s3_data_transfer(self.__osn_config)
 
     def make_stream(self):
         _, _, processed_data_filepaths = self.process_files_as_read()
@@ -30,8 +29,15 @@ class osn_stream_processor(DataFileStreamProcessor, Runnable):
         return self.n_msgs_read
 
     def _process_downloaded_data_file(self, datafile, lock):
-        s3d = s3_data_transfer(self.__osn_config)
-        s3d.transfer_object_stream(datafile)
+
+        self.s3d.transfer_object_stream(self.topic_name, datafile)
+
+        if self.s3d.compare_consumer_datafile_with_osn_object_stream(self.topic_name, self.bucket_name, datafile):
+            file_name = str(datafile.filename)
+            sub_dir = datafile.get_subdir_str
+            object_key = sub_dir + '/' + file_name
+            logging.info(object_key + ' matched with consumer datafile')
+            # self.s3d.delete_object_from_osn(self.bucket_name, object_key)
         return None
 
     @classmethod
@@ -56,12 +62,12 @@ class osn_stream_processor(DataFileStreamProcessor, Runnable):
         args = parser.parse_args(args=args)
 
         osn_stream_proc = cls(
-                              args.bucket_name,
-                              args.config, args.topic_name,
-                              n_threads=args.n_threads,
-                              update_secs=args.update_seconds,
-                              consumer_group_ID=args.consumer_group_ID,
-                              logger_file=args.logger_file)
+            args.bucket_name,
+            args.config, args.topic_name,
+            n_threads=args.n_threads,
+            update_secs=args.update_seconds,
+            consumer_group_ID=args.consumer_group_ID,
+            logger_file=args.logger_file)
 
         # cls.bucket_name = args.bucket_name
         msg = f'Listening to the {args.topic_name} topic to find Lecroy data files and create '
@@ -69,7 +75,7 @@ class osn_stream_processor(DataFileStreamProcessor, Runnable):
         # n_read, n_processed = osn_stream_proc.make_stream()
         n_read, n_processed = osn_stream_proc.make_stream()
         osn_stream_proc.close()
-        run_stop = datetime.datetime.now()
+        # run_stop = datetime.datetime.now()
         # shut down when that function returns
         msg = ''
         if args.output_dir is not None:
@@ -84,11 +90,10 @@ class osn_stream_processor(DataFileStreamProcessor, Runnable):
 
 def main(args=None):
     args = [
-        #  ' ',
-            ' ', '--config',
-            ' ',
-            '--topic_name', 'laser_topic']
-    #
+        '',
+        # '--root_dir', '',
+        '--config', ',
+        '--topic_name', '']
 
     osn_stream_processor.run_from_command_line(args)
 
