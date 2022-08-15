a5ae1ac7847cd4885b137ebb28b2b14569fe940f
Maggie Eminizer <margaret.eminizer@gmail.com>
2022-08-15 11:58:05 -0400

    update openmsistream version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setupkwargs = dict(
     install_requires=['gemd>=1.9.0',
                       'matplotlib',
                       'methodtools',
-                      'openmsistream>=0.9.1.4',
+                      'openmsistream>=0.9.3.0',
                       'pandas',
                       'python-fmrest>=1.4.0',
                       'scipy; python_version>="3.8"',
